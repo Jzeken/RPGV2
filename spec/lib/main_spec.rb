c750196d63b79de30c85c5efc51661063dddc06e
Jzeken <csyron.damian14@gmail.com>
2018-07-06 12:34:26 +0800

    Another update ..

--- spec/lib/main_spec.rb
+++ spec/lib/main_spec.rb
@@ -1,2 +1,18 @@
-class Main
+require 'spec_helper'
+require '../../lib/main'
+
+describe Main
+  describe ".duel" do
+    it " Initiate Duel process " do
+      w = Warrior.new
+      m = Mage.new
+      a = Deatmatch.new(w,m)
+
+      if w.hp == Warrior.new.hp
+        expect(m.hp) < 100
+      else
+        expect(w.hp) < 100
+      end
+    end
+  end
 end
