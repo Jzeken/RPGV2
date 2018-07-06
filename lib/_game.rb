6ed622622992d8cee0754e2e9cbeb87b562619f7
Jzeken <csyron.damian14@gmail.com>
2018-07-06 13:17:04 +0800

    Mess

--- lib/_game.rb
+++ lib/_game.rb
@@ -1,35 +1,35 @@
 class Game
   
-  def match(hero1, hero2)
-    attacker, defender = [hero1,hero2].shuffle        #<--- Random Turns
+  def match(hero1, hero2)                                               #=MATCH MAKING=
+    attacker, defender = [hero1,hero2].shuffle                          #<--- Random Turns
   end
 
-  def process(attacker,defender)
-    battle(attacker,defender, damage)
+  def process(attacker,defender)                                        #=BATTLE PROCESS=
+    battle(attacker,defender, damage)                                   #<--- STATUS COMPUTATION
     puts "#{attacker.name} dealt #{damage} to #{defender.name}"
     puts "==#{defender.name} Status=="
     puts "HP : #{defender.hp} || Armor : #{defender.armor}"
     puts "==#{attacker.name} Status=="
     puts "HP : #{attacker.hp} || Armor : #{attacker.armor}"
     
-    if defender.hp <= 0 then
-      puts "#{attacker.name} wins,"
+    if defender.hp <= 0 then                                            # Defender HP reach 0
+      puts "#{attacker.name} wins,"                                     #End Game
       puts "#{defender.name} lose. Game Over"
     end
 
     private 
-    def battle (attacker, deffender)
-      chance = rand (attacker.chance..100)                                  #<---Dealing Damage
-      if attacker.chance >= chance then
-        damage = defender.damage - attacker.damage + attacker.crit
-      elsif attacker.chance != chance then
-        damage = defender.damage - attacker.damage  
+    def battle (attacker, deffender)                                    #<---Dealing Damage
+      trigger = rand (attacker.chance..100)                             #<---randomize crit chance trigger
+      if attacker.chance >= trigger then                                # crit chance >= trigger chance
+        damage = defender.damage - attacker.damage + attacker.crit      # output damage with crit crit is True
+      elsif attacker.chance != trigger then                             # crit chance != trigger chance
+        damage = defender.damage - attacker.damage                      # output damage without crit crit is False
       end
-
-      if defender.armor <= 0 then                                            #<---Status
-        defender.hp - attacker.damage
-      elsif defender.armor > 0
-        deffender.armor - attacker.damage
+                                                                        #=Status=
+      if defender.armor <= 0 then                                       # Defender Armor <= 0   
+        defender.hp - attacker.damage                                   # Damage will be on HP
+      elsif defender.armor > 0                                          # Defender Armor > 0
+        deffender.armor - attacker.damage                               # Damage will be on HP
       end
     end
   end
--- lib/main.rb
+++ lib/main.rb
@@ -7,6 +7,6 @@ require "_tank"
 
 class Main
   def duel (hero1, hero2, mode)
-    a = mode.new(hero1,hero2)
+    x = mode.new(hero1,hero2)
   end
 end
